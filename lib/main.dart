93ca4ade4cf82d7cb2c67fb80cefdb3f68551cec
kyjzzang81 <kyjzzang81@gmail.com>
2022-06-07 15:54:44 +0900

    0607

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,88 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: MyApp()
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var like = [0, 0, 0, 0, 0];
+  var name = [
+    '아아아', '우우우', '가가가', '나나나나', '다다다'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // home : Image.asset('assets/tiger.png')
-      // home : SizedBox()
-      home : Scaffold(
-        appBar : AppBar( title: Text('상단 타이틀')),
-        body : Center(
-          child: Container(
-            width: 50, height: 50,
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              border: Border.all(color: Colors.black)
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialog(context: context, builder: (context){
+            return DialogEx();
+          });
+        },
+      ),
+      appBar: AppBar(title: Text('연락처앱'),),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (c, i){
+
+          return ListTile(
+            leading: Text(like[i].toString()),
+            title: Text(name[i]),
+            trailing: ElevatedButton(
+              child: Text("좋아요"),
+              onPressed: () {
+                setState(() {
+                  like[i]++;
+                });
+              },
             ),
-          ),
-        ),
+            contentPadding: EdgeInsets.all(5),
+          );
+        },
+      ),
+    );
+  }
+}
 
 
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 100,
-            child: Row(
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          )
-        ),
-      )
+class DialogEx extends StatelessWidget {
+  const DialogEx({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("제목"),
+      content: Text("내용입니다."),
+
+      actionsOverflowButtonSpacing: 20,
+      actions: [
+        ElevatedButton(onPressed: (){
+          Navigator.of(context).pop();
+        }, child: Text("Back")),
+        ElevatedButton(onPressed: (){
+        }, child: Text("Next")),
+      ],
+
+      titleTextStyle: TextStyle(fontWeight : FontWeight.bold, color:Colors.black, fontSize: 20),
+      contentTextStyle: TextStyle(fontSize: 14, color: Colors.black87),
+      backgroundColor: Colors.amber,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20))
+      ),
     );
   }
 }
